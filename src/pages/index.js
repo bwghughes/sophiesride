5ede250452737ac5dee94e422078b10baac218ec
Ben Hughes <ben.hughes@apple.com>
2019-07-29 14:47:35 +0100

    Initial import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,26 @@
 import React from "react"
+import SophieImg from "../data/images/sophie.png"
 
-export default () => <div>Hello world!</div>
+export default () => 
+<div style={{ margin: `3rem auto`, maxWidth: 600, textAlign: 'center' }}>
+    <img src={SophieImg} alt="Image of Sophie"></img>
+    <h1>Sophie's Ride 2019</h1>
+    <p>
+      The bi-annual pilgrimage to Rainbow's Hospice will be on the 21st September 2019.
+    </p>
+
+    <hr></hr>
+    <h3>Donations</h3>
+    <p>
+      Please dontate to the just giving page <a href="https://www.justgiving.com/fundraising/jane-rushton6">here</a>.
+    </p>
+    <hr></hr>
+    <h3>The Details</h3>
+    <p>
+      <h4>Time</h4>  
+      <p>9am Sharp.</p>
+
+      <h4>Address</h4>  
+      <p><a href="https://goo.gl/maps/BVchVV3bCygJ8xb9A">The Little Car Park, Barton Marina, Barton Under Needwood, Burton On Trent</a></p>
+    </p>
+  </div>
