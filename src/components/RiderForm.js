d1fd05d63c928e0842e39bf4ef9dcad4cd73c571
Ben Hughes <ben.hughes@apple.com>
2019-07-30 13:46:57 +0100

    Added team size

--- src/components/RiderForm.js
+++ src/components/RiderForm.js
@@ -9,23 +9,28 @@ export default props => (
     </div>
     <div class="row">
       <div class="col-sm-12">
-        <div class="form-group">
-          <form action="/thanks/" name="rider" method="post" data-netlify="true">
+        <form action="/thanks/" name="rider" method="post" data-netlify="true">
           <input type="hidden" name="form-name" value="rider" />
+          <div class="form-group">
             <label>
               Name:&nbsp;
-              <input type="text" name="name" id="name" autoFocus />
+              <input type="text" name="name" id="name" class="form-control" autoFocus />
             </label>
-            &nbsp;&nbsp;&nbsp;
+          </div>
+          <div class="form-group">
             <label>
               Mobile #:&nbsp;
-              <input type="mobile" name="mobile" id="mobile" />
+              <input type="mobile" name="mobile" id="mobile" class="form-control"/>
             </label>
-            <br />
-            <br />
-            <button type="submit">I'm In!</button>
-          </form>
-        </div>
+          </div>
+          <div class="form-group">
+            <label>
+              Number in Group:&nbsp;
+              <input type="number" name="number" id="number" class="form-control"/>
+            </label>
+          </div>
+          <button type="submit" class="btn btn-success">I'm In!</button>
+        </form>
       </div>
     </div>
   </div>
